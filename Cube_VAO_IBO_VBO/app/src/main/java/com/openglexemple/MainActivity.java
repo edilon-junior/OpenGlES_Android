aad10c84506155af49dd84cf276d5784a816dae9
edilon-junior <93557226+edilon-junior@users.noreply.github.com>
2021-11-02 12:56:28 -0300

    Create MainActivity.java

---
+++ Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.openglexemple;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class MainActivity extends Activity
+{
+    /** Hold a reference to our GLSurfaceView */
+    private MySurfaceView mGLSurfaceView;
+    private Loader loader;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        mGLSurfaceView = new MySurfaceView(this);
+
+        // Check if the system supports OpenGL ES 3.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x30002;
+
+        if (supportsEs2)
+        {
+            // Request an OpenGL ES 3.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(3);
+
+            final DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new MyRenderer(this), displayMetrics.density);
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
+
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+}
