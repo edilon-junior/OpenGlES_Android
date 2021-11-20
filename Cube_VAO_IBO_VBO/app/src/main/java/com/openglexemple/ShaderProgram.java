3f233d6c130bb193d38795dd032b0be1513d6b19
edilon-junior <93557226+edilon-junior@users.noreply.github.com>
2021-11-20 20:19:51 -0300

    Update ShaderProgram.java

--- Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/ShaderProgram.java
+++ Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/ShaderProgram.java
@@ -1,7 +1,16 @@
 package com.example.openglexemple;
 
-import android.opengl.GLES20;
-import android.opengl.GLES32;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINK_STATUS;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES30.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES30.GL_VERTEX_SHADER;
+import static android.opengl.GLES30.glEnableVertexAttribArray;
+import static android.opengl.GLES30.glGetShaderiv;
+import static android.opengl.GLES30.glVertexAttribPointer;
+
+import android.opengl.GLES30;
 import android.util.Log;
 
 import java.nio.FloatBuffer;
@@ -18,10 +27,20 @@ public class ShaderProgram {
     private final Map<String, Integer> floatUniformHandles;
     private final Map<String, Integer> attributeHandles;
 
-    public ShaderProgram(final String vertexString, final String fragmentString, final String[] attributes){
+    private String vertexName;
+    private String fragmentName;
+
+    public ShaderProgram(final String vertexName, final String fragmentName, final String[] attributes, Loader loader){
+        this.vertexName = vertexName;
+        this.fragmentName = fragmentName;
+
         intUniformHandles = new HashMap<>();
         floatUniformHandles = new HashMap<>();
         attributeHandles = new HashMap<>();
+
+        String vertexString = loader.shaderToString(vertexName);
+        String fragmentString = loader.shaderToString(fragmentName);
+
         programHandle = createShaderProgram(vertexString, fragmentString, attributes);
     }
     /**
@@ -31,40 +50,52 @@ public class ShaderProgram {
      */
     public int createShaderProgram(final String vertexString, final String fragmentString, final String[] attributes)
     {
-        final int vertexShaderHandle   = compileShader(GLES20.GL_VERTEX_SHADER, vertexString);
-        final int fragmentShaderHandle = compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentString);
+        final int vertexShaderHandle   = compileShader(GL_VERTEX_SHADER, vertexString);
+        final int fragmentShaderHandle = compileShader(GL_FRAGMENT_SHADER, fragmentString);
         int programHandle = createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle,
                 attributes);
         return programHandle;
     }
 
-    private static int  compileShader(final int shaderType, final String shaderSource)
+    private int  compileShader(final int shaderType, final String shaderSource)
     {
-        int shaderHandle = GLES20.glCreateShader(shaderType);
+        int shaderHandle = GLES30.glCreateShader(shaderType);
 
         if (shaderHandle != 0)
         {
             // Pass in the shader source.
-            GLES20.glShaderSource(shaderHandle, shaderSource);
+            GLES30.glShaderSource(shaderHandle, shaderSource);
 
             // Compile the shader.
-            GLES20.glCompileShader(shaderHandle);
+            GLES30.glCompileShader(shaderHandle);
 
             // Get the compilation status.
             int[] compileStatus = new int[1];
-            GLES20.glGetShaderiv(shaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+            glGetShaderiv(shaderHandle, GLES30.GL_COMPILE_STATUS, compileStatus, 0);
 
             // If the compilation failed, delete the shader.
             if (compileStatus[0] == 0)
             {
-                Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shaderHandle));
-                GLES20.glDeleteShader(shaderHandle);
+                if(shaderType == GL_VERTEX_SHADER){
+                    Log.d(TAG, "Load "+vertexName+" failed: "+GLES30.glGetShaderInfoLog(shaderHandle));
+
+                }else{
+                    Log.d(TAG, "Load "+fragmentName+" failed: "+GLES30.glGetShaderInfoLog(shaderHandle));
+                }
+
+                GLES30.glDeleteShader(shaderHandle);
                 shaderHandle = 0;
             }
         }
 
         if (shaderHandle == 0)
         {
+            if(shaderType == GL_VERTEX_SHADER){
+                Log.d(TAG, "Load "+vertexName+"  failed. Creation");
+            }else{
+                Log.d(TAG, "Load "+fragmentName+"  failed. Creation");
+            }
+
             throw new RuntimeException("Error creating shader.");
         }
 
@@ -79,39 +110,39 @@ public class ShaderProgram {
      * @param attributes Attributes that need to be bound to the program.
      * @return An OpenGL handle to the program.
      */
-    private static int createAndLinkProgram(int vertexShaderHandle, int fragmentShaderHandle, String[] attributes)
+    private int createAndLinkProgram(int vertexShaderHandle, int fragmentShaderHandle, String[] attributes)
     {
-        int programHandle = GLES20.glCreateProgram();
+        int programHandle = GLES30.glCreateProgram();
 
         if (programHandle != 0)
         {
             // Bind the vertex shader to the program.
-            GLES20.glAttachShader(programHandle, vertexShaderHandle);
+            GLES30.glAttachShader(programHandle, vertexShaderHandle);
 
             // Bind the fragment shader to the program.
-            GLES20.glAttachShader(programHandle, fragmentShaderHandle);
+            GLES30.glAttachShader(programHandle, fragmentShaderHandle);
 
             // Bind attributes
             if (attributes != null)
             {
                 for (int i = 0; i < attributes.length; i++)
                 {
-                    GLES20.glBindAttribLocation(programHandle, i, attributes[i]);
+                    GLES30.glBindAttribLocation(programHandle, i, attributes[i]);
                 }
             }
 
             // Link the two shaders together into a program.
-            GLES20.glLinkProgram(programHandle);
+            GLES30.glLinkProgram(programHandle);
 
             // Get the link status.
             final int[] linkStatus = new int[1];
-            GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
+            GLES30.glGetProgramiv(programHandle, GL_LINK_STATUS, linkStatus, 0);
 
             // If the link failed, delete the program.
             if (linkStatus[0] == 0)
             {
-                Log.e(TAG, "Error compiling program: " + GLES20.glGetProgramInfoLog(programHandle));
-                GLES20.glDeleteProgram(programHandle);
+                Log.e(TAG, "Error compiling program: " + GLES30.glGetProgramInfoLog(programHandle));
+                GLES30.glDeleteProgram(programHandle);
                 programHandle = 0;
             }
         }
@@ -129,19 +160,22 @@ public class ShaderProgram {
     }
 
     public void useProgram(){
-        GLES20.glUseProgram(programHandle);
+        GLES30.glUseProgram(programHandle);
     }
 
-    public void passAttributesBuffers(String[] attributes, int[] sizes, FloatBuffer[] attribBuffers) {
+    public void enableVertexAttribArray(String[] attributes, int[] sizes, FloatBuffer[] attribBuffers) {
         for(int i=0; i<attributes.length; i++) {
             String attributeName = attributes[i];
             int handle = attributeHandles.get(attributeName);
+
             if(attribBuffers[i]==null){
                 //Log.i(TAG, attributeName+" has null data");
             }else{
-                GLES20.glVertexAttribPointer(handle, sizes[i], GLES20.GL_FLOAT, false,
+                glEnableVertexAttribArray(handle);
+                //attribBuffers[i].position(0);
+                glVertexAttribPointer(handle, sizes[i], GL_FLOAT, false,
                         0, attribBuffers[i]);
-                GLES20.glEnableVertexAttribArray(handle);
+
             }
         }
     }
@@ -149,34 +183,44 @@ public class ShaderProgram {
     public void enableVertexAttribArray(String attributeName, int size, int stride, int pointer){
         int handle = attributeHandles.get(attributeName);
 
-        GLES32.glEnableVertexAttribArray(handle);
+        glEnableVertexAttribArray(handle);
 
-        GLES32.glVertexAttribPointer(handle, size, GLES20.GL_FLOAT, false,
+        glVertexAttribPointer(handle, size, GL_FLOAT, false,
                 stride, pointer);
-
     }
 
     public void setIntUniformHandles(final String[] intUniforms) {
         for (String uniformName : intUniforms) {
-            int handle = GLES20.glGetUniformLocation(programHandle, uniformName);
+            int handle = glGetUniformLocation(programHandle, uniformName);
             intUniformHandles.put(uniformName, handle);
         }
     }
     public void setFloatUniformHandles(final String[] floatUniforms) {
         for (String uniformName : floatUniforms) {
-            int handle = GLES20.glGetUniformLocation(programHandle, uniformName);
+            int handle = glGetUniformLocation(programHandle, uniformName);
             floatUniformHandles.put(uniformName, handle);
         }
     }
 
     public void setAttributeHandles(final String[] attributes){
         for(String attribName : attributes){
-            int handle =GLES20.glGetAttribLocation(programHandle, attribName);
+            int handle = glGetAttribLocation(programHandle, attribName);
+            //System.out.println(attribName +": "+handle);
             attributeHandles.put(attribName,handle);
         }
     }
 
-    // flatUniforms and datas must be in same order
+    public void passAttrib3f(String name, float[] attribute){
+        int handle = attributeHandles.get(name);
+        GLES30.glVertexAttrib3f(handle, attribute[0], attribute[1], attribute[2]);
+    }
+
+    public void passAttrib3f(String name, Vector3f attribute){
+        int handle = attributeHandles.get(name);
+        GLES30.glVertexAttrib3f(handle, attribute.x, attribute.y, attribute.z);
+    }
+
+    // flatUniforms and data must be in same order
     public void passFloatUniforms(final String[] floatUniforms,  final float[]... uniforms){
         for(int i = 0; i<floatUniforms.length;i++){
             int handle = floatUniformHandles.get(floatUniforms[i]);
@@ -193,46 +237,46 @@ public class ShaderProgram {
 
     public void passIntUniform(final int handle, final int[] data){
         if(data.length == 1){
-            GLES20.glUniform1i(handle, data[0]);
+            GLES30.glUniform1i(handle, data[0]);
         }
     }
 
     public void passFloatUniform(int handle, final float[] data){
         if(data.length == 1){
-            GLES20.glUniform1f(handle, data[0]);
+            GLES30.glUniform1f(handle, data[0]);
         }
         if(data.length == 2){
-            GLES20.glUniform2f(handle, data[0], data[1]);
+            GLES30.glUniform2f(handle, data[0], data[1]);
         }
         if(data.length == 3){
-            GLES20.glUniform3f(handle, data[0], data[1], data[2]);
+            GLES30.glUniform3f(handle, data[0], data[1], data[2]);
         }
         if(data.length == 4){
-            GLES20.glUniform4f(handle, data[0], data[1], data[2], data[3]);
+            GLES30.glUniform4f(handle, data[0], data[1], data[2], data[3]);
         }
         if(data.length == 16){
-            GLES20.glUniformMatrix4fv(handle, 1, false, data, 0);
+            GLES30.glUniformMatrix4fv(handle, 1, false, data, 0);
         }
     }
 
     public void passUniformMatrix(int handle , final float[] data){
-        GLES20.glUniformMatrix4fv(handle, 1, false, data, 0);
+        GLES30.glUniformMatrix4fv(handle, 1, false, data, 0);
     }
     public void passUniformFloat3(int handle, final float[] data){
-        GLES20.glUniform3f(handle, data[0], data[1], data[2]);
+        GLES30.glUniform3f(handle, data[0], data[1], data[2]);
     }
     public void passUniformFloat2(int handle, final float[] data){
-        GLES20.glUniform2f(handle, data[0], data[1]);
+        GLES30.glUniform2f(handle, data[0], data[1]);
     }
     public void passUniformFloat1(int handle, final float[] data){
-        GLES20.glUniform1f(handle, data[0]);
+        GLES30.glUniform1f(handle, data[0]);
     }
 
     public void disableAttributes(){
         for (int handle : attributeHandles.values()) {
-            GLES20.glDisableVertexAttribArray(handle);
+            if(handle != -1){
+                GLES30.glDisableVertexAttribArray(handle);
+            }
         }
-
     }
-
 }
