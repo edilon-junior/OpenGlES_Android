dcb73d1255116ca9934e84ce6215e3b417baef43
edilon-junior <93557226+edilon-junior@users.noreply.github.com>
2021-12-09 18:48:57 -0300

    Update MyRenderer.java

--- Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/MyRenderer.java
+++ Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/MyRenderer.java
@@ -1,8 +1,10 @@
 package com.example.openglexemple;
 
-import java.nio.FloatBuffer;
-import java.util.ArrayList;
-import java.util.List;
+import static com.example.openglexemple.Constants.POINT_ATTRIBUTES;
+import static com.example.openglexemple.Constants.POINT_FLOAT_UNIFORMS;
+import static com.example.openglexemple.Constants.SCENE_ATTRIBUTES;
+import static com.example.openglexemple.Constants.SCENE_FLOAT_UNIFORMS;
+import static com.example.openglexemple.Constants.SCENE_INT_UNIFORMS;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -11,6 +13,7 @@ import android.content.Context;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 import android.os.SystemClock;
+import android.util.Log;
 
 public class MyRenderer implements GLSurfaceView.Renderer
 {
@@ -20,69 +23,13 @@ public class MyRenderer implements GLSurfaceView.Renderer
     private static final String TAG = "RENDERER";
 
     private final Context mActivityContext;
-
-    public static final String ATTRIBUTE_POSITION = "a_Position";
-    public static final String ATTRIBUTE_NORMAL = "a_Normal";
-    public static final String ATTRIBUTE_TEXTURE_COORDINATE = "a_TexCoordinate";
-    public static final String ATTRIBUTE_COLOR = "a_Color";
-    public static final String UNIFORM_LIGHT_POS = "u_LightPos";
-    public static final String UNIFORM_LIGHT_COLOR = "u_LightColor";
-    public static final String UNIFORM_VIEW_POS = "u_ViewPos";
-    public static final String UNIFORM_AMBIENT = "u_Ambient";
-    public static final String UNIFORM_DIFFUSE = "u_Diffuse";
-    public static final String UNIFORM_SPECULAR = "u_Specular";
-    public static final String UNIFORM_SHININESS = "u_Shininess";
-    public static final String UNIFORM_MV_MATRIX = "u_MVMatrix";
-    public static final String UNIFORM_MVP_MATRIX = "u_MVPMatrix";
-    public static final String UNIFORM_TEXTURE = "u_Texture";
-    public static final String UNIFORM_COLOR = "u_Color";
-    public static final String UNIFORM_POINT_SIZE = "u_PointSize";
-
-    public static final int POSITION_SIZE = 3;
-    public static final int NORMAL_SIZE = 3;
-    public static final int TEXTURE_COORDINATE_SIZE = 2;
-    public static final int COLOR_SIZE = 4;
-    public static final int BYTES_PER_FLOAT = 4;
-    public static final int BYTES_PER_INT = 4;
-    public static final int STRIDE_T = (POSITION_SIZE + NORMAL_SIZE + TEXTURE_COORDINATE_SIZE) * BYTES_PER_FLOAT;
-    public static final int STRIDE_C = (POSITION_SIZE + NORMAL_SIZE + COLOR_SIZE) * BYTES_PER_FLOAT;
-
-
-    private final String[] sceneFloatUniforms = new String[]{
-            UNIFORM_LIGHT_POS, UNIFORM_LIGHT_COLOR, UNIFORM_VIEW_POS,
-            UNIFORM_AMBIENT, UNIFORM_DIFFUSE, UNIFORM_SPECULAR, UNIFORM_SHININESS,
-            UNIFORM_MV_MATRIX, UNIFORM_MVP_MATRIX
-    };
-
-    public final String[] sceneIntUniforms = new String[]{
-            UNIFORM_TEXTURE
-    };
-
-    public final String[] sceneAttributes = new String[]{
-            ATTRIBUTE_POSITION, ATTRIBUTE_NORMAL, ATTRIBUTE_TEXTURE_COORDINATE, ATTRIBUTE_COLOR
-    };
-
-    public int[] sceneAttributesSizes = new int[]{
-            POSITION_SIZE, NORMAL_SIZE, TEXTURE_COORDINATE_SIZE, COLOR_SIZE
-    };
-
-    private final String[] pointAttributes = new String[] {ATTRIBUTE_POSITION};
-    private final String[] pointFloatUniforms = new String[]{UNIFORM_MVP_MATRIX, UNIFORM_COLOR, UNIFORM_POINT_SIZE};
-
-
-    private final float[] lightColor = new float[]{1, 1, 1};
-
-    private float[] mLightPosInEyeSpace = new float[4];
-
-
-    ShaderProgram sceneProgram;
-    ShaderProgram pointProgram;
     private final Transformation transformation;
     private final Loader loader;
     private Scene scene;
-
     float initial_time = 0;
     boolean start = false;
+    private ShaderProgram pointProgram;
+    //private ShaderProgram sceneProgram;
 
     public MyRenderer(final Context activityContext)
     {
@@ -94,19 +41,26 @@ public class MyRenderer implements GLSurfaceView.Renderer
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config)
     {
-        sceneProgram = new ShaderProgram("per_pixel_vertex_shader.vert", "per_pixel_fragment_shader.frag", sceneAttributes, loader);
-        pointProgram = new ShaderProgram("point_vertex_shader.vert", "point_fragment_shader.frag", pointAttributes, loader);
+        //sceneProgram = new ShaderProgram("per_pixel_vertex_shader.vert", "per_pixel_fragment_shader.frag", SCENE_ATTRIBUTES, loader);
+       // sceneProgram.setAttributeHandles(SCENE_ATTRIBUTES);
+        //sceneProgram.setFloatUniformHandles(SCENE_FLOAT_UNIFORMS);
+        //sceneProgram.setIntUniformHandles(SCENE_INT_UNIFORMS);
 
-        setupSceneProgram();
-        setupPointProgram()
-        
-        scene = new Scene(loader, sceneProgram);
+        pointProgram = new ShaderProgram("point_vertex_shader.vert", "point_fragment_shader.frag", POINT_ATTRIBUTES, loader);
+        pointProgram.setAttributeHandles(POINT_ATTRIBUTES);
+        pointProgram.setFloatUniformHandles(POINT_FLOAT_UNIFORMS);
+
+        scene = new Scene(loader, transformation);
+        scene.setPointProgram(pointProgram);
+        scene.createGameObjects(loader);
         // Set the background clear color.
         GLES30.glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 
+        GLES30.glLineWidth(4);
+
         // Use culling to remove back faces.
         GLES30.glEnable(GLES30.GL_CULL_FACE);
-        //GLES20.glCullFace(GLES20.GL_FRONT);
+        GLES30.glCullFace(GLES30.GL_BACK);
         // Enable depth testing
         GLES30.glEnable(GLES30.GL_DEPTH_TEST);
 
@@ -139,64 +93,62 @@ public class MyRenderer implements GLSurfaceView.Renderer
             start = true;
         }
         float time = (SystemClock.uptimeMillis() - initial_time) / 1000; //time in seconds
-       
-        pointProgram.useProgram();
-        for(PointObject point : scene.getPointObjectList()){
-            point.update(time);
-            drawLight(point);
 
-            mLightPosInEyeSpace = transformation.convertIntoEyeSpace(point.getModelPosition(), point.getModelMatrix());
+        //Draw a point to indicate the light.
+
+        //ShaderProgram pointProgram = scene.getPointProgram();
+        pointProgram.useProgram();
+        pointProgram.setAttributeHandles(POINT_ATTRIBUTES);
+        pointProgram.setFloatUniformHandles(POINT_FLOAT_UNIFORMS);
+        for(Point point : scene.getPointList()){
+            point.update(time);
+            point.render(pointProgram, transformation);
             point.cleanUp();
         }
-
+        for(Polygon polygon : scene.getPolygonList()){
+            polygon.update(time);
+            polygon.render(pointProgram, transformation);
+            polygon.cleanUp();
+        }
+       /*
         sceneProgram.useProgram();
         for(GameObject gameObject: scene.getGameObjectList()){
             gameObject.getMesh().setupTexture();
-
             gameObject.update(time);
-            drawVAOCube(gameObject);
+            gameObject.render(scene, transformation);
             gameObject.cleanUp();
         }
-
+*/
         deltaX = 0.0f;
         deltaY = 0.0f;
     }
-
-    private void drawVAOCube(GameObject cube)
+/*
+    private void drawCube(GameObject cube)
     {
         float[] mMVPMatrix = transformation.getMVPMatrix(cube.getModelMatrix());
+
+        FloatBuffer bPosition = cube.getMesh().getBufferPositions();
+        FloatBuffer bNormal = cube.getMesh().getBufferNormals();
+        FloatBuffer bTexture = cube.getMesh().getBufferTextureCoordinates();
+        FloatBuffer bColor = cube.getMesh().getBufferColors();
+        FloatBuffer[] attribBuffers = new FloatBuffer[]{bPosition, bNormal, bTexture, bColor};
 
         float[] ambient = cube.getMesh().getMaterial().ambient;
         float[] diffuse = cube.getMesh().getMaterial().diffuse;
         float[] specular = cube.getMesh().getMaterial().specular;
         float[] shininess = cube.getMesh().getMaterial().shininess;
 
+        sceneProgram.enableVertexAttribArray(sceneAttributes,
+                sceneAttributesSizes, attribBuffers);
         sceneProgram.passIntUniforms(sceneIntUniforms, new int[]{0});
         sceneProgram.passFloatUniforms(sceneFloatUniforms, mLightPosInEyeSpace, lightColor, new float[]{0,0,0},
                 ambient, diffuse, specular, shininess,
                 transformation.getViewMatrix(), mMVPMatrix);
 
-        cube.getMesh().render(GLES30.GL_TRIANGLES);
+        cube.getMesh().draw();
+
+        sceneProgram.disableAttributes();
     }
+*/
 
-    private void drawPoint(PointObject pointObject)
-    {
-
-        float[] mMVPMatrix = transformation.getMVPMatrix(pointObject.getModelMatrix());
-
-        pointProgram.passFloatUniforms(pointFloatUniforms, mMVPMatrix, pointObject.getColor(), pointObject.getSize());
-
-        pointObject.getMesh().render(GLES30.GL_POINTS);
-    }
-
-    public void setupSceneProgram(){
-        sceneProgram.setAttributeHandles(sceneAttributes);
-        sceneProgram.setFloatUniformHandles(sceneFloatUniforms);
-        sceneProgram.setIntUniformHandles(sceneIntUniforms);
-    }
-
-    public void setupPointProgram(){
-        pointProgram.setAttributeHandles(pointAttributes);
-        pointProgram.setFloatUniformHandles(pointFloatUniforms);
-    }
 }
