4dea241aeb04753ca343365b82445ad0f6f31a11
edilon-junior <93557226+edilon-junior@users.noreply.github.com>
2021-12-11 17:14:35 -0300

    Update Point.java

--- Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Point.java
+++ Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Point.java
@@ -7,29 +7,15 @@ import android.opengl.Matrix;
 
 public class Point extends GameObject{
     private float[] modelPosition =  new float[] {0.0f, 0.0f, 0.0f, 1.0f};
-    private float[] color;
-    private final float[] size = new float[1];
 
     public Point(Vector3f position, float[] color, float size, ShaderProgram shaderProgram){
         super();
         setPosition(position);
         setColor(color);
-        setSize(size);
+        setPointSize(size);
         Mesh pointMesh = new Mesh(new float[]{0,0,0}, null, null, null, null, null);
         pointMesh.setupNonInterleavedMesh(shaderProgram);
         setMesh(pointMesh);
-    }
-
-    public void setColor(float[] color) {
-        this.color = color;
-    }
-
-    public void setSize(float size) {
-        this.size[0] = size;
-    }
-
-    public float[] getSize(){
-        return this.size;
     }
 
     public void updateModelMatrix(){
@@ -49,14 +35,14 @@ public class Point extends GameObject{
 
     @Override
     public void render(ShaderProgram shaderProgram, Transformation transformation){
-        System.out.println("render from pointObject class");
         float[] mMVPMatrix = transformation.getMVPMatrix(getModelMatrix());
-        Utils.printMatrix(getModelMatrix());
-        shaderProgram.passFloatUniforms(POINT_FLOAT_UNIFORMS, mMVPMatrix, this.color, this.size);
+        shaderProgram.passFloatUniforms(POINT_FLOAT_UNIFORMS, mMVPMatrix, getColor(), getPointSize());
         getMesh().render(GLES30.GL_POINTS);
     }
 
 }
+
+
 
 
 
