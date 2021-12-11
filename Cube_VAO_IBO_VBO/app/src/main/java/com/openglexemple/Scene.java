ac11d2fc2d3b2a23d781d27b17b13d2df433c325
edilon-junior <93557226+edilon-junior@users.noreply.github.com>
2021-12-11 17:15:42 -0300

    Update Scene.java

--- Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Scene.java
+++ Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Scene.java
@@ -1,6 +1,8 @@
 package com.example.openglexemple;
 
 import static com.example.openglexemple.Colors.GREEN;
+import static com.example.openglexemple.Colors.RED;
+import static com.example.openglexemple.Colors.WHITE;
 import static com.example.openglexemple.Constants.POINT_ATTRIBUTES;
 import static com.example.openglexemple.Constants.POINT_FLOAT_UNIFORMS;
 import static com.example.openglexemple.Constants.SCENE_ATTRIBUTES;
@@ -16,9 +18,8 @@ public class Scene {
     private final String TAG = "SCENE";
 
     private final Map<Integer, ShaderProgram> shaderProgramList = new HashMap<>();
-    private final List<GameObject> gameObjectList = new ArrayList<>();
-    private final List<Point> pointList = new ArrayList<>();
-    private final List<Polygon> polygonList = new ArrayList<>();
+    private final List<GameObject> solidModelList = new ArrayList<>();
+    private final List<GameObject> pointList = new ArrayList<>();
 
     public Light getLight() {
         return light;
@@ -59,76 +60,74 @@ public class Scene {
     }
 
     public void createGameObjects(Loader loader){
-        GameObject cube = new GameObject("cube_dice", loader, sceneProgram, 0);
+        GameObject cube = new SolidModel("cube_dice", loader, sceneProgram, 0);
         //cube.setAngularVelocity(360/10);
         //cube.setRotationAxis(0,1,0);
         cube.translate(-3,6,-10);
         cube.setRotation(0,0,1,0);
         addGameObject(cube);
 
-        GameObject cube2 = new GameObject("cube_dice", loader, sceneProgram, 0);
+        GameObject cube2 = new SolidModel("cube_dice", loader, sceneProgram, 0);
         //cube2.setAngularVelocity(360/10);
         //cube2.setRotationAxis(1,0,0);
         cube2.translate(-3, 0, -10);
         cube2.setRotation(90,0,1,0);
         addGameObject(cube2);
 
-        GameObject cube3 = new GameObject("cube_dice", loader, sceneProgram, 0);
+        GameObject cube3 = new SolidModel("cube_dice", loader, sceneProgram, 0);
         //cube2.setAngularVelocity(360/10);
         //cube2.setRotationAxis(1,0,0);
         cube3.translate(-3, -6, -10);
         cube3.setRotation(180,0,1,0);
         addGameObject(cube3);
 
-        GameObject cube4 = new GameObject("cube_dice", loader, sceneProgram, 0);
+        GameObject cube4 = new SolidModel("cube_dice", loader, sceneProgram, 0);
         //cube2.setAngularVelocity(360/10);
         //cube2.setRotationAxis(1,0,0);
         cube4.translate(3, 6, -10);
         cube4.setRotation(270,0,1,0);
         addGameObject(cube4);
 
-        GameObject cube5 = new GameObject("cube_dice", loader, sceneProgram, 0);
+        GameObject cube5 = new SolidModel("cube_dice", loader, sceneProgram, 0);
         //cube2.setAngularVelocity(360/10);
         //cube2.setRotationAxis(1,0,0);
         cube5.translate(3, 0, -10);
         cube5.setRotation(90,1,0,0);
         addGameObject(cube5);
 
-        GameObject cube6 = new GameObject("cube_dice", loader, sceneProgram, 0);
+        GameObject cube6 = new SolidModel("cube_dice", loader, sceneProgram, 0);
         //cube2.setAngularVelocity(360/10);
         //cube2.setRotationAxis(1,0,0);
         cube6.translate(3, -6, -10);
         cube6.setRotation(270,1,0,0);
         addGameObject(cube6);
 
-        Point point1 = new Point(new Vector3f(2,5,-6), new float[]{1f, 1f, 1f, 1.0f}, 10.0f, pointProgram);
-        Point point2 = new Point(new Vector3f(0,0,-6), new float[]{1f, 0, 0, 1.0f}, 10.0f, pointProgram);
+        GameObject point1 = new Point(new Vector3f(2,5,-6), WHITE, 10.0f, pointProgram);
+        GameObject point2 = new Point(new Vector3f(0,0,-6), RED, 10.0f, pointProgram);
+
+        GameObject polygon = new Polygon(new Vector3f(0,0,-6), 16, GREEN, 1,0, pointProgram);
+        polygon.setPointSize(10);
+
         pointList.add(point1);
         pointList.add(point2);
-
-        Polygon polygon = new Polygon(16, 10.0f, GREEN, 1,0, pointProgram);
-        polygonList.add(polygon);
+        pointList.add(polygon);
 
     }
 
     public void addGameObject(GameObject gameObject){
-        gameObjectList.add(gameObject);
+        solidModelList.add(gameObject);
     }
 
     public void addGameObject(GameObject gameObject, int index){
-        gameObjectList.add(index, gameObject);
+        solidModelList.add(index, gameObject);
     }
 
     public List<GameObject> getGameObjectList(){
-        return this.gameObjectList;
+        return this.solidModelList;
     }
 
-    public List<Point> getPointList(){
+    public List<GameObject> getPointList(){
         return this.pointList;
-    }
-
-    public List<Polygon> getPolygonList(){
-        return this.polygonList;
     }
 
     public ShaderProgram getShaderProgram(int shaderProgramId){
