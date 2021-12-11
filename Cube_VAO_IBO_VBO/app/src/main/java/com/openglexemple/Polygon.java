047c3ddbd15ed89696f85165ba30bb4d38f81312
edilon-junior <93557226+edilon-junior@users.noreply.github.com>
2021-12-11 17:14:10 -0300

    Update Polygon.java

--- Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Polygon.java
+++ Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Polygon.java
@@ -6,64 +6,44 @@ import static com.example.openglexemple.Constants.POSITION_SIZE;
 import android.opengl.GLES30;
 import android.opengl.Matrix;
 
-public class Polygon {
+public class Polygon extends GameObject{
 
-    private float side_length = 0;
+    private float sideLength = 0;
     private float radius = 0;
-    private float[] color;
-    private float[] size = new float[1];
-    private float[] modelMatrix = new float[16];
-    private int shaderProgramId = -1;
-    private Vector3f position = new Vector3f();
-    private Vector3f scale = new Vector3f(1,1,1);
-    private Mesh mesh;
-    private boolean updateModelMatrix = false;
 
     /**
+     * @param position: define initial position
  * @param sides: define the number of sides
- * @param size: size of point in OpenGL shader program
  * @param color: color of lines
  * @param side_length: define length of side. If this param is grater than zero, then radius shall be zero.
  * @param radius: The distance from the center of a regular polygon to any vertex .
  * @param shaderProgram: shader program used by this object
  **/
 
-    public Polygon(int sides, float size, float[] color, float side_length, float radius, ShaderProgram shaderProgram){
+    public Polygon(Vector3f position, int sides, float[] color, float side_length, float radius, ShaderProgram shaderProgram){
+        super();
 
         if(side_length > 0 ){
-            this.side_length = side_length;
+            this.sideLength = side_length;
             this.radius = calcRadius(sides, side_length);
         }else{
-            this.side_length = calcSideLength(sides, radius);
+            this.sideLength = calcSideLength(sides, radius);
             this.radius = radius;
         }
 
+        setPosition(position);
         setShaderProgramId(shaderProgram.getProgramHandle());
-        setPosition(new Vector3f(0,0,-6));
         setColor(color);
-        setSize(size);
         setScale(new Vector3f(radius, radius, radius));
 
         float[] positions = createVertices(sides);
-        mesh = new Mesh(positions, null, null, null, null, null);
-        mesh.setupNonInterleavedMesh(shaderProgram);
-    }
-
-    private void setScale(Vector3f scale) {
-        this.scale.set(scale);
-    }
-
-    private void setPosition(Vector3f position) {
-        this.position.set(position);
-        updateModelMatrix = true;
-    }
-
-    private void setShaderProgramId(int programHandle) {
-        this.shaderProgramId = programHandle;
+        Mesh polygonMesh = new Mesh(positions, null, null, null, null, null);
+        polygonMesh.setupNonInterleavedMesh(shaderProgram);
+        setMesh(polygonMesh);
     }
 
     private float calcRadius(int sides, float side_length){
-        return (float) (side_length/(2*Math.sin(Math.PI / sides)));
+        return (float) (side_length / (2 * Math.sin(Math.PI / sides)));
     }
 
     private float calcSideLength(int sides, float radius){
@@ -84,57 +64,29 @@ public class Polygon {
             vertices[i*POSITION_SIZE + 1] = (float) (1 * Math.sin((i+1) * angle));
             vertices[i*POSITION_SIZE + 2] = 0;
         }
-        System.out.println("polygon vertices:");
-        Utils.printFloatArray(vertices);
+
         return vertices;
     }
-    public void setColor(float[] color){
-        this.color = color;
-    }
 
-    public float[] getColor(){
-        return this.color;
-    }
-
-    public void setSize(float size){
-        this.size[0] = size;
-    }
-
-    public float[] getSize(){
-        return this.size;
-    }
-
-    private int getShaderProgramId() {
-        return this.shaderProgramId;
-    }
-
-    private float[] getModelMatrix() {
-        return this.modelMatrix;
-    }
-
-    public void translate(float x, float y, float z){
-        this.position.keepAdd(x,y,z);
-        Matrix.translateM(this.modelMatrix, 0,this.position.x,this.position.y,this.position.z);
-    }
     public void updateModelMatrix(){
-        if(updateModelMatrix == true){
-            Matrix.setIdentityM(modelMatrix, 0);
-            Matrix.translateM(modelMatrix, 0,position.x,position.y,position.z);
+        if(updateModelMatrix){
+            float[] tempMatrix = new float[16];
+            Matrix.setIdentityM(tempMatrix, 0);
+            Matrix.translateM(tempMatrix, 0,getPosition().x,getPosition().y,getPosition().z);
+            setModelMatrix(tempMatrix);
             updateModelMatrix = false;
         }
     }
 
+    @Override
     public void update(float time){
         updateModelMatrix();
     }
 
+    @Override
     public void render(ShaderProgram shaderProgram, Transformation transformation){
-        float[] mMVPMatrix = transformation.getMVPMatrix(this.modelMatrix);
-        shaderProgram.passFloatUniforms(POINT_FLOAT_UNIFORMS, mMVPMatrix, this.color, this.size);
-        this.mesh.render(GLES30.GL_LINE_LOOP);
-    }
-
-    public void cleanUp(){
-
+        float[] mMVPMatrix = transformation.getMVPMatrix(getModelMatrix());
+        shaderProgram.passFloatUniforms(POINT_FLOAT_UNIFORMS, mMVPMatrix, getColor(), getPointSize());
+        getMesh().render(GLES30.GL_LINE_LOOP);
     }
 }
