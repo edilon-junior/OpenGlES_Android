5a6539d6700501dea0e24e804c81f1b75efbb708
edilon-junior <93557226+edilon-junior@users.noreply.github.com>
2021-12-11 16:31:59 -0300

    Update Point.java

--- Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Point.java
+++ Cube_VAO_IBO_VBO/app/src/main/java/com/openglexemple/Point.java
@@ -1,156 +1,63 @@
 package com.example.openglexemple;
 
+import static com.example.openglexemple.Constants.POINT_FLOAT_UNIFORMS;
+
+import android.opengl.GLES30;
 import android.opengl.Matrix;
 
-public abstract class GameObject {
+public class Point extends GameObject{
+    private float[] modelPosition =  new float[] {0.0f, 0.0f, 0.0f, 1.0f};
+    private float[] color;
+    private final float[] size = new float[1];
 
-    protected boolean updateModelMatrix;
-    private int meshIndex;
-    private int shaderProgramId;
-    private float angularDisplacement;
-    private float angularVelocity;
-    private float initialRotTime;
-    private final float currentRotTime;
-    protected final float[]  modelMatrix;
-    private final Vector3f scale;
-    private final Vector3f initialPosition;
-    private final Vector3f position;
-    private final Vector3f rotationAxis;
-    private final Vector3f translationalVelocity;
-    protected Mesh mesh;
-
-    public GameObject(){
-        updateModelMatrix = false;
-        meshIndex = -1;
-        shaderProgramId = -1;
-        initialPosition = new Vector3f();
-        position = new Vector3f();
-        rotationAxis = new Vector3f(1,0,0);
-        translationalVelocity = new Vector3f();
-        angularDisplacement = 0.0f;
-        scale= new Vector3f(1.0f, 1.0f, 1.0f);
-        modelMatrix = new float[16];
-        angularVelocity = 0;
-        initialRotTime = 0;
-        currentRotTime = 0;
-        Matrix.setIdentityM(modelMatrix, 0);
+    public Point(Vector3f position, float[] color, float size, ShaderProgram shaderProgram){
+        super();
+        setPosition(position);
+        setColor(color);
+        setSize(size);
+        Mesh pointMesh = new Mesh(new float[]{0,0,0}, null, null, null, null, null);
+        pointMesh.setupNonInterleavedMesh(shaderProgram);
+        setMesh(pointMesh);
     }
 
-    public void setScale(Vector3f scale){
-        this.scale.set(scale);
-        updateModelMatrix = true;
-    }
-    public Vector3f getScale(){
-        return this.scale.clone();
-    }
-    public void setShaderProgramId(int shaderProgramId){
-        this.shaderProgramId = shaderProgramId;
-    }
-    public int getShaderProgramId(){
-        return shaderProgramId;
-    }
-    public void setMesh(Mesh mesh){
-        this.mesh = mesh;
-    }
-    public Mesh getMesh(){
-        return this.mesh;
-    }
-    public void setMeshIndex(int index){
-        this.meshIndex = index;
-    }
-    public int getMeshIndex(){
-        return this.meshIndex;
-    }
-    public void setInitialRotTime(float initialRotTime){
-        this.initialRotTime = initialRotTime;
-    }
-    public float getInitialRotTime() {
-        return initialRotTime;
-    }
-    public float getAngularDisplacement() {
-        return angularDisplacement;
-    }
-    public float getAngularVelocity(){
-        return angularVelocity;
-    }
-    public Vector3f getRotationAxis(){
-        return this.rotationAxis.clone();
+    public void setColor(float[] color) {
+        this.color = color;
     }
 
-    public void setAngularVelocity(float angular_velocity){
-        this.angularVelocity = angular_velocity;
-    }
-    public void setRotationAxis(float x, float y, float z){
-        this.rotationAxis.set(x,y,z);
+    public void setSize(float size) {
+        this.size[0] = size;
     }
 
-    public void setModelMatrix(float[] modelMatrix){
-        for(int i= 0; i<modelMatrix.length; i++){
-            this.modelMatrix[i] = modelMatrix[i];
-        }
+    public float[] getSize(){
+        return this.size;
     }
 
-    public float[] getModelMatrix(){
-        return this.modelMatrix;
-    }
-
-    public void setAngularDisplacement(float angleInDegrees){
-        this.angularDisplacement = angleInDegrees;
-    }
-
-    public void setPosition(Vector3f position){
-        this.position.set(position);
-        updateModelMatrix = true;
-    }
-
-    public Vector3f getPosition(){
-        return this.position.clone();
-    }
-
-    public void setRotation(float angle, float x, float y, float z){
-        setRotationAxis(x,y,z);
-        setAngularDisplacement(angle);
-        updateModelMatrix = true;
-    }
-    public void translate(float x, float y, float z){
-        this.position.keepAdd(x,y,z);
-        Matrix.translateM(this.modelMatrix, 0,this.position.x,this.position.y,this.position.z);
-    }
-
-    public void rotate(float increment_angle, float x, float y, float z){
-        setRotationAxis(x,y,z);
-        setAngularDisplacement(increment_angle);
-        Matrix.rotateM(modelMatrix, 0, angularDisplacement, rotationAxis.x, rotationAxis.y, rotationAxis.z);
-    }
-
-    public void update(float time){
-        updateModelMatrix();
-        //translate first!
-        updateRotation(time);
-    }
-
-    private void updateModelMatrix(){
-        if(updateModelMatrix == true){
-            Matrix.setIdentityM(modelMatrix, 0);
-            Matrix.scaleM(modelMatrix, 0, scale.x, scale.y, scale.z);
-            Matrix.rotateM(modelMatrix, 0, angularDisplacement, rotationAxis.x, rotationAxis.y, rotationAxis.z);
-            Matrix.translateM(modelMatrix, 0,position.x,position.y,position.z);
+    public void updateModelMatrix(){
+        if(updateModelMatrix){
+            float[] tempMatrix = new float[16];
+            Matrix.setIdentityM(tempMatrix, 0);
+            Matrix.translateM(tempMatrix, 0,getPosition().x,getPosition().y,getPosition().z);
+            setModelMatrix(tempMatrix);
             updateModelMatrix = false;
         }
     }
 
-    public void updateRotation(float time){
-
+    @Override
+    public void update(float time){
+        updateModelMatrix();
     }
 
-    public void render(Scene scene, ShaderProgram shaderProgram, Transformation transformation){
-
-    }
+    @Override
     public void render(ShaderProgram shaderProgram, Transformation transformation){
-
+        System.out.println("render from pointObject class");
+        float[] mMVPMatrix = transformation.getMVPMatrix(getModelMatrix());
+        Utils.printMatrix(getModelMatrix());
+        shaderProgram.passFloatUniforms(POINT_FLOAT_UNIFORMS, mMVPMatrix, this.color, this.size);
+        getMesh().render(GLES30.GL_POINTS);
     }
-    public void cleanUp(){
 
-    }
 }
+
+
+
 
